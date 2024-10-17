3ce106c51222ba391ab4aa140933280694be1d15
ali dev14 <ali.nrdn14005@gmail.com>
2024-10-17 21:26:32 +0700

    fixed

--- _public/modules/modul_rcsa/rcsa/controllers/Rcsa.php
+++ _public/modules/modul_rcsa/rcsa/controllers/Rcsa.php
@@ -691,7 +691,6 @@ if($dtkri){
 		$id = intval($this->uri->segment(4));
 		$data['parent'] = $this->db->where('id', $id)->get(_TBL_VIEW_RCSA)->row_array();
 		$data['field'] = $this->data->get_peristiwa($id);
-		
 		$this->template->build('fom_peristiwa', $data);
 	}
 
@@ -722,23 +721,16 @@ if($dtkri){
 		foreach ($rows as $row) {
 			$data['sasaran'][$row['id']] = $row['sasaran'];
 		}
-		$rows_bisnis = $this->db->where('rcsa_no',$id_rcsa)->get(_TBL_RCM)->result_array();
-		$data['proses_bisnis'] = ['- select -'];
-		foreach ($rows_bisnis as $rb) {
-			$data['proses_bisnis'][$rb['id']] = $rb['bussines_process'];
-		}
 
 		$couse = [];
 		$impact = [];
 		$detail = [];
 		$sub = [];
 		$event = [];
-		$rcsa_det = [];
+		// doi::dump($id_edit);
 		if ($id_edit > 0) {
-			
+
 			$detail = $this->db->where('id', $id_edit)->get(_TBL_VIEW_RCSA_DETAIL)->row_array();
-			$rcsa_det = $this->db->where('id', $id_edit)->get(_TBL_RCSA_DETAIL)->row_array();
-			// doi::dump($rcsa_det);
 			if ($detail["sts_propose"] == 4) {
 				$disabled = 'disabled';
 				$readonly = 'readonly="true"';
@@ -868,7 +860,6 @@ if($dtkri){
 		}
 		
 		$action = $this->db->where('rcsa_detail_no', $id_edit)->get(_TBL_RCSA_ACTION)->row_array();
-		$data["rcsa_det"] = $rcsa_det;
 		$data['field'] = $action;
 		$data['id_edit_mitigasi'] = $action['id'];
 
@@ -905,24 +896,6 @@ if($dtkri){
 
 		// echo json_encode($result);
 
-	}
-
-	function update_sts_heatmap(){
-		$id   	= $this->input->post('id');
-		$status = $this->input->post('status');
-		// Validasi ID dan status
-        if (is_numeric($id) && ($status == 0 || $status == 1)) {
-
-			$result = $this->data->save_status($id, $status);
-            // Memanggil model untuk mengupdate status
-            if ($result) {
-                echo "Data berhasil di Update!";
-            } else {
-                echo "Data gagal di Update";
-            }
-        } else {
-            echo "Data tidak valid!";
-        }
 	}
 	function add_peristiwa()
 	{
@@ -1824,6 +1797,12 @@ if($dtkri){
 			$cboTreatment1 = $this->get_combo('treatment1');
 			$cboTreatment2 = $this->get_combo('treatment2');
 
+			if(isset($post['mode'])){
+				if(isset($post['month'])){
+					$result['month'] = $post['month'];
+				}
+				$result['mode'] = $post['mode'];
+ 			}
 			if ($result['level_name'] == "Ekstrem") {
 				$result['level_resiko'] = $cboTreatment1;
 			} elseif ($result['level_name'] == "Low") {
