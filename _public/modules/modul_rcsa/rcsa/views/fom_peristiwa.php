83ba557e3a1d31dcf967522a6e350ff778ec17fa
teguh triatmojo <teguhtriatmojo23@gmail.com>
2024-10-17 23:41:06 +0700

    update fix

--- _public/modules/modul_rcsa/rcsa/views/fom_peristiwa.php
+++ _public/modules/modul_rcsa/rcsa/views/fom_peristiwa.php
@@ -61,14 +61,14 @@ if ($field['iskri'] == 0) {
 }
 
 // test 
-$pb = [
-    '' => '- Pilih Proses Bisnis -',
-    'proses bisnis 1' => 'Pembelian',
-    'penjualan' => 'Penjualan',
-    'produksi' => 'Produksi',
-    'distribusi' => 'Distribusi',
-    'logistik' => 'Logistik',
-];
+// $pb = [
+//     '' => '- Pilih Proses Bisnis -',
+//     'proses bisnis 1' => 'Pembelian',
+//     'penjualan' => 'Penjualan',
+//     'produksi' => 'Produksi',
+//     'distribusi' => 'Distribusi',
+//     'logistik' => 'Logistik',
+// ];
 ?>
 
 
@@ -189,15 +189,10 @@ $pb = [
 
                                         <tr>
                                             <td width="20%">Proses Bisnis</td>
-                                            <td colspan="2"><?= form_dropdown('proses_bisnis', $pb, ($detail) ? $detail['proses_bisnis'] : '', 'class="select2 form-control" style="width:100%;" id="proses_bisnis"' . $disable); ?></td>
+                                            <td colspan="2"><?= form_dropdown('proses_bisnis', $proses_bisnis, ($rcsa_det) ? $rcsa_det['rcm_id'] : '', 'class="select2 form-control" style="width:100%;" id="proses_bisnis"' . $disable); ?></td>
                                         </tr>
 
-                                        <tr>
-                                            <td width="20%">Tampilkan di Heatmap</td>
-                                            <td colspan="2"><?= form_checkbox('sts_heatmap', 'sts_heatmap', 1, ($detail) ? $detail['sts_heatmap'] : '', 'class="select2 form-control form-check-input" style="width:100%;"' . $disable); ?></td>
-                                        </tr>
-
-
+                
                                         <tr>
                                             <td width="20%" rowspan="3">Peristiwa (T3)</td>
                                         </tr>
@@ -373,7 +368,7 @@ $pb = [
                                                         <td width="25%">Asumsi Perhitungan Dampak </td>
                                                         <td>
                                                             <div id="risk_asumsi_perhitungan_dampak" class="input-group">
-                                                                <?= form_input('risk_asumsi_perhitungan_dampak', ($detail) ? ($detail['risk_asumsi_perhitungan_dampak']) : '', 'class="form-control text-right" style="width:100%; id="risk_asumsi_perhitungan_dampak"' . $disable); ?>
+                                                            <?= form_input('risk_asumsi_perhitungan_dampak', ($rcsa_det) ? ($rcsa_det['risk_asumsi_perhitungan_dampak']) : '', 'class="form-control text-right" style="width:100%; id="risk_asumsi_perhitungan_dampak"' . $disable); ?>
                                                             </div>
                                                         </td>
                                                     </tr>
