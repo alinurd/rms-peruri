c5d0ed82a8e0e6cff9b19a99bf14311920cbc876
teguh triatmojo <teguhtriatmojo23@gmail.com>
2024-10-24 16:14:46 +0700

    update

--- _public/modules/modul_rcsa/level_risiko/controllers/Level_Risiko.php
+++ _public/modules/modul_rcsa/level_risiko/controllers/Level_Risiko.php
@@ -210,7 +210,7 @@ class Level_Risiko extends BackendController
 			$where['id']                    = $id;
 			$result = $this->crud->crud_data(array('table' => _TBL_RCSA_DETAIL, 'field' => $upd, 'where' => $where, 'type' => 'update'));
 		}
-		echo json_encode($id);
+		echo json_encode($result);
 		// 	var_dump($simpan);
 		// exit;
 		// echo "<script>
