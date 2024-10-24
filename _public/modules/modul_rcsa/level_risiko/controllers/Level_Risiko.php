b9bf8b68f6f0b5b0b4a1ebc1ac3eda7d2d270f37
Ilmanmr <ali.nrdn14005@gmail.com>
2024-10-24 02:56:10 +0000

    fix level risiko

--- _public/modules/modul_rcsa/level_risiko/controllers/Level_Risiko.php
+++ _public/modules/modul_rcsa/level_risiko/controllers/Level_Risiko.php
@@ -3,7 +3,7 @@
 //NamaTbl, NmFields, NmTitle, Type, input, required, search, help, isiedit, size, label 
 //$tbl, 'id', 'id', 'int', false, false, false, true, 0, 4, 'l_id'
 
-class level_risiko extends BackendController
+class Level_Risiko extends BackendController
 {
 	var $type_risk = 0;
 	var $table = "";
