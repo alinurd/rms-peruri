46fedebbb2fb9ee01173ba5928321a6e08824820
ali dev14 <ali.nrdn14005@gmail.com>
2024-10-29 02:02:35 +0700

    ok

--- _public/modules/modul_parameter/index_komposit/models/Data.php
+++ _public/modules/modul_parameter/index_komposit/models/Data.php
@@ -8,38 +8,62 @@ class Data extends MX_Model {
 	}
 	
 	 
-	function save_privilege($newid=0,$data=array(), $old_data=array())
-	{
-		if (isset($data['urut'])){
- 				$no=0;
-				 foreach ($data['urut'] as $index => $urut) {
-					$upd = [
-						'id_combo' => $newid,
-						'urut' => $urut,
-						'parameter' => $data['param'][$index],
-						'skala' => $data['skala'][$index],
-						'penilaian' => $data['penilaian'][$index]
-					];
+		
+	function save_privilege($newid=0, $data=array(), $old_data=array())
+{
+     if (isset($data['urut'])) {
+        $no = 0;
+        foreach ($data['urut'] as $index => $urut) {
+            // Data utama yang akan disimpan
+            $upd = [
+                'id_combo' => $newid,
+                'urut' => $urut,
+                'parameter' => $data['param'][$index],
+                'skala' => $data['skala'][$index],
+                'penilaian' => $data['penilaian'][$index]
+            ];
+            
+            // Cek apakah data utama sudah ada
+            $x = $this->db->where('id_combo', $newid)->where('urut', $urut)->get("bangga_index_komposit")->row_array();
 
-					$x = $this->db->where('id_combo', $newid)->where('urut', $urut)->get("bangga_index_komposit")->row_array();
+            if (intval($x['id'])) {
+                $upd['update_user'] = $this->authentication->get_Info_User('username');
+                $where = ['id' => $x['id']];
+                $this->crud->crud_data(array('table' => "bangga_index_komposit", 'field' => $upd, 'where' => $where, 'type' => 'update'));
+            } else {
+                $upd['create_user'] = $this->authentication->get_Info_User('username');
+                $this->crud->crud_data(array('table' => "bangga_index_komposit", 'field' => $upd, 'type' => 'add'));
+                $id = $this->db->insert_id(); // Mengambil ID terakhir yang disimpan
+            }
 
-						if(intval($x['id']))
-						{
-							$upd['update_user'] = $this->authentication->get_Info_User('username');
-							$where['id'] = $x['id'];
- 							$this->crud->crud_data(array('table'=>"bangga_index_komposit", 'field'=>$upd, 'where'=>$where,'type'=>'update'));
-						}
-						else
-						{
-							$upd['create_user'] = $this->authentication->get_Info_User('username');
-							$this->crud->crud_data(array('table'=>"bangga_index_komposit", 'field'=>$upd,'type'=>'add'));
-						}
-					}
+             // Menyimpan data detail
+            if (isset($data['detail_urut'][$urut])) {
+                foreach ($data['detail_urut'][$urut] as $detailIndex => $detailUrut) {
+                    $updDetail = [
+                        'id_param' => $id,
+                        'urut' => $detailUrut,
+                        'parameter' => $data['detail_param'][$urut][$detailIndex],
+                        'skala' => $data['detail_skala'][$urut][$detailIndex],
+                        'penilaian' => $data['detail_penilaian'][$urut][$detailIndex]
+                    ];
 
-		 
-			} 
-		return true;
-	}
+                    // Cek apakah data detail sudah ada
+                    $xDetail = $this->db->where('id_param', $id)->where('urut', $detailUrut)->get("bangga_index_komposit_detail")->row_array();
+                    if (intval($xDetail['id'])) {
+                        $updDetail['update_user'] = $this->authentication->get_Info_User('username');
+                        $whereDetail = ['id' => $xDetail['id']];
+                        $this->crud->crud_data(array('table' => "bangga_index_komposit_detail", 'field' => $updDetail, 'where' => $whereDetail, 'type' => 'update'));
+                    } else {
+                        $updDetail['create_user'] = $this->authentication->get_Info_User('username');
+                        $this->crud->crud_data(array('table' => "bangga_index_komposit_detail", 'field' => $updDetail, 'type' => 'add'));
+                    }
+                }
+            }
+        }
+     }
+    return true;
+}
+
 }
 /* End of file app_login_model.php */
 /* Location: ./application/models/app_login_model.php */
--- _public/modules/modul_parameter/index_komposit/views/param.php
+++ _public/modules/modul_parameter/index_komposit/views/param.php
@@ -9,102 +9,165 @@
         </tr>
     </thead>
     <tbody>
-        <?php foreach ($data as $p): ?>
-        <tr>
-            <td><input type="text" class="form-control" name="urut[]" value="<?= $p['urut'] ?>" readonly></td>
-            <td><input class="form-control" type="text" name="param[]" value="<?= $p['parameter'] ?>" id="param"></td>
-            <td><input class="form-control" type="number" name="skala[]" value="<?= $p['skala'] ?>" id="skala"></td>
-            <td><input class="form-control" type="number" name="penilaian[]" value="<?= $p['penilaian'] ?>" id="penilaian"></td>
+    <?php foreach($data as $p):
+            $details = $this->db
+        ->where('id_param', $p['id'])
+        ->get('bangga_index_komposit_detail')
+        ->result_array(); ?>
+
+        <tr class="main-row">
+            <td rowspan="1"><input type="text" class="form-control" name="urut[]" value="<?= $p['urut'] ?>" readonly></td>
+            <td><input class="form-control" type="text" name="param[]" value="<?= $p['parameter'] ?>" placeholder="Parameter"></td>
+            <td><input class="form-control" type="number" name="skala[]"  value="<?= $p['skala'] ?>"placeholder="Skala"></td>
+            <td><input class="form-control" type="number" name="penilaian[]"  value="<?= $p['penilaian'] ?>"placeholder="Hasil Penilaian"></td>
             <td>
+                <button type="button" class="btn btn-info add-Detail"><i class="fa fa-plus"></i></button>
                 <button type="button" class="btn btn-danger delete-row"><i class="fa fa-trash"></i></button>
             </td>
         </tr>
         <?php endforeach; ?>
+
     </tbody>
 </table>
 
 <center>
     <button id="addParam" class="btn btn-primary" type="button">
-        <i class="fa fa-plus"></i>
-    </button>
+        <i class="fa fa-plus"></i> Tambah
+    </button> 
 </center>
 
 <script>
-document.getElementById("addParam").addEventListener("click", function() {
-    const tbody = document.querySelector("#tbl_sasaran_new tbody");
-    const rowCount = tbody.rows.length;
-    const urutan = String.fromCharCode(65 + rowCount); // Urutan abjad, misalnya A, B, C, dst.
-
-    const newRow = document.createElement("tr");
-
-    const urutCell = document.createElement("td");
-    const urutInput = document.createElement("input");
-    urutInput.className = "form-control";
-    urutInput.type = "text";
-    urutInput.name = "urut[]";
-    urutInput.value = urutan;
-    urutInput.readOnly = true;
-    urutCell.appendChild(urutInput);
-    newRow.appendChild(urutCell);
-
-    const paramCell = document.createElement("td");
-    const paramInput = document.createElement("input");
-    paramInput.className = "form-control";
-    paramInput.type = "text";
-    paramInput.name = "param[]";
-    paramCell.appendChild(paramInput);
-    newRow.appendChild(paramCell);
-
-    const skalaCell = document.createElement("td");
-    const skalaInput = document.createElement("input");
-    skalaInput.className = "form-control";
-    skalaInput.type = "number";
-    skalaInput.name = "skala[]";
-    skalaCell.appendChild(skalaInput);
-    newRow.appendChild(skalaCell);
-
-    const penilaianCell = document.createElement("td");
-    const penilaianInput = document.createElement("input");
-    penilaianInput.className = "form-control";
-    penilaianInput.type = "number";
-    penilaianInput.name = "penilaian[]";
-    penilaianCell.appendChild(penilaianInput);
-    newRow.appendChild(penilaianCell);
-
-    const aksiCell = document.createElement("td");
-    const deleteButton = document.createElement("button");
-    deleteButton.className = "btn btn-danger delete-row";
-    deleteButton.innerHTML = '<i class="fa fa-trash"></i>';
-    deleteButton.type = "button";
-    aksiCell.appendChild(deleteButton);
-    newRow.appendChild(aksiCell);
-
-    tbody.appendChild(newRow);
-    updateUrutAbjad();
- 
-    deleteButton.addEventListener("click", function() {
-        newRow.remove();
-        updateUrutAbjad();
-    });
-});
-
+// Fungsi memperbarui urutan dan rowspan kolom utama
 function updateUrutAbjad() {
     const tbody = document.querySelector("#tbl_sasaran_new tbody");
-    const rows = tbody.querySelectorAll("tr");
+    const rows = tbody.querySelectorAll(".main-row");
 
     rows.forEach((row, index) => {
         const urutInput = row.querySelector('input[name="urut[]"]');
         urutInput.value = String.fromCharCode(65 + index);
+        updateRowspan(row);
     });
 }
- 
-document.querySelectorAll(".delete-row").forEach(button => {
+
+// Fungsi memperbarui rowspan kolom utama
+function updateRowspan(mainRow) {
+    const urutCell = mainRow.querySelector("td");
+    let detailCount = 0;
+    let nextRow = mainRow.nextElementSibling;
+
+    while (nextRow && nextRow.classList.contains("detail-row")) {
+        detailCount++;
+        nextRow = nextRow.nextElementSibling;
+    }
+    urutCell.rowSpan = detailCount + 1;
+}
+
+// Fungsi untuk memperbarui urutan nomor detail
+function updateDetailUrut(mainRow) {
+    let detailRows = [];
+    let nextRow = mainRow.nextElementSibling;
+
+    while (nextRow && nextRow.classList.contains("detail-row")) {
+        detailRows.push(nextRow);
+        nextRow = nextRow.nextElementSibling;
+    }
+
+    detailRows.forEach((row, index) => {
+        row.querySelector(".detail-urut input").value = index + 1;
+    });
+}
+
+// Event listener untuk menambah detail baru
+document.querySelectorAll(".add-Detail").forEach(button => {
     button.addEventListener("click", function() {
-        if (confirm("Apakah Anda yakin ingin menghapus data?")) {
-            button.closest("tr").remove();
-            updateUrutAbjad();
-        }
+        const mainRow = button.closest("tr");
+        const lastDetailRow = findLastDetailRow(mainRow) || mainRow; // Dapatkan baris detail terakhir
+
+        const newRow = document.createElement("tr");
+        newRow.classList.add("detail-row");
+
+         newRow.innerHTML = `
+            <td class="detail-urut">
+                <input type="text" class="form-control" name="detail_urut[]" readonly style="width: 5%; display: inline-block; margin-right: 5px;">
+                <input type="text" class="form-control" name="detail_param[]" placeholder="Detail Parameter" style="width: 90%; display: inline-block;">
+            </td>
+            <td><input class="form-control" type="number" name="detail_skala[]" placeholder="Skala"></td>
+            <td><input class="form-control" type="number" name="detail_penilaian[]" placeholder="Hasil Penilaian"></td>
+            <td><button type="button" class="btn btn-warning delete-detail-row"><i class="fa fa-trash"></i></button></td>
+        `;
+
+        lastDetailRow.insertAdjacentElement('afterend', newRow); 
+        updateDetailUrut(mainRow);
+        updateRowspan(mainRow);
+
+         newRow.querySelector(".delete-detail-row").addEventListener("click", function() {
+            newRow.remove();
+            updateDetailUrut(mainRow);
+            updateRowspan(mainRow);
+        });
     });
 });
 
+ function findLastDetailRow(mainRow) {
+    let nextRow = mainRow.nextElementSibling;
+
+    while (nextRow && nextRow.classList.contains("detail-row")) {
+        if (!nextRow.nextElementSibling || !nextRow.nextElementSibling.classList.contains("detail-row")) {
+            return nextRow;
+        }
+        nextRow = nextRow.nextElementSibling;
+    }
+    return null;
+}
+
+ document.getElementById("addParam").addEventListener("click", function() {
+    const tbody = document.querySelector("#tbl_sasaran_new tbody");
+    const rowCount = tbody.querySelectorAll(".main-row").length;
+    const urutan = String.fromCharCode(65 + rowCount); 
+
+    const newRow = document.createElement("tr");
+    newRow.classList.add("main-row");
+
+     newRow.innerHTML = `
+        <td rowspan="1"><input type="text" class="form-control" name="urut[]" value="${urutan}" readonly></td>
+        <td><input class="form-control" type="text" name="param[]"></td>
+        <td><input class="form-control" type="number" name="skala[]"></td>
+        <td><input class="form-control" type="number" name="penilaian[]"></td>
+        <td>
+            <button type="button" class="btn btn-info add-Detail"><i class="fa fa-plus"></i></button>
+            <button type="button" class="btn btn-danger delete-row"><i class="fa fa-trash"></i></button>
+        </td>
+    `;
+
+    tbody.appendChild(newRow);
+
+     newRow.querySelector(".add-Detail").addEventListener("click", function() {
+        const lastDetailRow = findLastDetailRow(newRow) || newRow; 
+
+        const detailRow = document.createElement("tr");
+        detailRow.classList.add("detail-row");
+
+        detailRow.innerHTML = `
+            <td class="detail-urut">
+                <input type="text" class="form-control" name="detail_urut[${urutan}[]" readonly style="width: 5%; display: inline-block; margin-right: 5px;">
+                <input type="text" class="form-control" name="detail_param[${urutan}[]" placeholder="Detail Parameter" style="width: 90%; display: inline-block;">
+            </td>
+            <td><input class="form-control" type="number" name="detail_skala[${urutan}[]" placeholder="Skala"></td>
+            <td><input class="form-control" type="number" name="detail_penilaian[${urutan}[]" placeholder="Hasil Penilaian"></td>
+            <td><button type="button" class="btn btn-warning delete-detail-row"><i class="fa fa-trash"></i></button></td>
+        `;
+
+        lastDetailRow.insertAdjacentElement('afterend', detailRow); 
+        updateDetailUrut(newRow);
+        updateRowspan(newRow);
+
+         detailRow.querySelector(".delete-detail-row").addEventListener("click", function() {
+            detailRow.remove();
+            updateDetailUrut(newRow);
+            updateRowspan(newRow);
+        });
+    });
+
+    updateUrutAbjad();
+});
 </script>
