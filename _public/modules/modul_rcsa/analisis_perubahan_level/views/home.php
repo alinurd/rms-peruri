4c40904aa6d28f7ab11441ff468a46a880e3c9e1
ali dev14 <ali.nrdn14005@gmail.com>
2024-10-31 04:09:06 +0700

    sisa simpan keterangan

--- _public/modules/modul_rcsa/analisis_perubahan_level/views/home.php
+++ _public/modules/modul_rcsa/analisis_perubahan_level/views/home.php
@@ -2,7 +2,7 @@
     <div class="col-md-12">
         <div class="row">
             <div class="col-sm-8 panel-heading">
-                <h3 style="padding-left:10px;"><?= lang("msg_title")?></h3>
+                <h3 style="padding-left:10px;"><?= lang("msg_title") ?></h3>
             </div>
             <div class="col-sm-4" style="text-align:right">
                 <ul class="breadcrumb">
@@ -16,7 +16,8 @@
 
 <section class="x_panel">
     <div class="x_title">
-        <form method="GET" action="<?= site_url($this->modul_name.'/index'); ?>">
+        <form method="GET" action="<?= site_url('analisis-perubahan-level/index'); ?>">
+
             <div class="row">
                 <div class="col-md-2 col-sm-4 col-xs-6">
                     <label for="filter_periode">Tahun</label>
@@ -36,13 +37,30 @@
                     </select>
                 </div>
 
+                <input type="hidden" name="triwulan" id="filter_triwulan">
                 <div class="col-md-2 col-sm-2 col-xs-2">
-                    <label for="filter_triwulan">Triwulan</label>
-                    <select name="triwulan" id="filter_triwulan" class="form-control select2" style="width: 80%;">
-                        <option value="1" <?= ($triwulan == 1) ? 'selected' : ''; ?>>Triwulan 1</option>
-                        <option value="2" <?= ($triwulan == 2) ? 'selected' : ''; ?>>Triwulan 2</option>
-                        <option value="3" <?= ($triwulan == 3) ? 'selected' : ''; ?>>Triwulan 3</option>
-                        <option value="4" <?= ($triwulan == 4) ? 'selected' : ''; ?>>Triwulan 4</option>
+
+                    <label for="filter_triwulan">Bulan</label><br>
+                    <select name="bulan" id="filter_bulan" class="form-control select2" style="width: 80%;" onchange="updateTriwulan()">
+                        <?php
+                        $bln=$this->input->get('bulan');
+                        $bulanName = [
+                            1 => 'Januari',
+                            'Februari',
+                            'Maret',
+                            'April',
+                            'Mei',
+                            'Juni',
+                            'Juli',
+                            'Agustus',
+                            'September',
+                            'Oktober',
+                            'November',
+                            'Desember'
+                        ];
+                        foreach ($bulanName as $key => $nama): ?>
+                            <option value="<?= $key; ?>" <?= ($bln == $key) ? 'selected' : ''; ?>><?= $nama; ?></option>
+                        <?php endforeach; ?>
                     </select>
                 </div>
 
@@ -75,18 +93,18 @@
                                     <th class="text-center" style="position: sticky; left: 360px; background: white; z-index: 99; width: 50px;">Inh</th>
                                     <th class="text-center" style="position: sticky; left: 400px; background: white; z-index: 99; width: 50px;">Res</th>
                                     <th class="text-center" style="position: sticky; left: 340px; background: white; z-index: 99; width: 50px;">PL</th>
-                                    <th class="text-center">keterangan Pergerakan Level Bulan :<?=$triwulan?></th>
-                                      
+                                    <th class="text-center">keterangan Pergerakan Level Bulan : <?=$bulanName[$bln] ?></th>
+
                                 </tr>
                             </thead>
                             <tbody>
                                 <?php
                                 $no = 1;
                                 foreach ($field as $q) {
-                                     $residual_level = $this->data->get_master_level(true, $q['inherent_level']);
+                                    $residual_level = $this->data->get_master_level(true, $q['inherent_level']);
                                     $getKode        = $this->data->level_action($residual_level['likelihood'], $residual_level['impact']);
                                     $reKod    = $getKode['like']['code'] . ' x ' . $getKode['impact']['code'];
-                                    
+
                                     $inherent = '
                                     <a class="btn" data-toggle="popover" data-content="
                                     <center>
@@ -94,10 +112,9 @@
                                     ' . $residual_level['level_mapping'] . '
                                     </center>
                                     " style="padding:4px; height:4px 8px;width:100%;background-color:' . $residual_level['color'] . ';color:' . $residual_level['color_text'] . ';"> &nbsp;</a>';
-                                  
-                                    $pl='';
-                                    $bln=1;
-                                    $getMonitoring=$this->data->getMonthlyMonitoringGlobal($q['id'], $bln, $residual_level['level_mapping']);
+
+                                    $pl = ''; 
+                                    $getMonitoring = $this->data->getMonthlyMonitoringGlobal($q['id'], $bln, $residual_level['level_mapping']);
                                 ?>
                                     <tr>
                                         <td style="position: sticky; left: 0; background: white;z-index: 99;"><?= $no++ ?></td>
@@ -107,14 +124,14 @@
                                         <td style="position: sticky; left: 360px; background: white; z-index: 95; "><?= $inherent ?></td>
                                         <td style="position: sticky; left:400px; background: white; z-index: 95; "><?= $getMonitoring['lv'] ?></td>
                                         <td style="position: sticky; left:440px; background: white; z-index: 95; ">
-                                       <?= $getMonitoring['pl']?>
-                    
-                                        </td>
-                                   
-                                            <td class="text-center">
-                                            <?= $getMonitoring['ket']?>
+                                            <?= $getMonitoring['pl'] ?>
 
-                                            </td> 
+                                        </td>
+
+                                        <td class="text-center">
+                                            <?= $getMonitoring['ket'] ?>
+
+                                        </td>
                                     </tr>
                                 <?php } ?>
                             </tbody>
@@ -132,17 +149,31 @@
                 <i><?= $timeLoad ?> second</i>
             </div>
         </div
-    </div>
+            </div>
 </section>
 
-<style>
-
-button {
-  background-color: transparent;
-  border: none;
-  box-shadow: none;
-  padding: 2;
-}
 
 
-</style>
+<script>
+    document.getElementById('filter_bulan').addEventListener('change', updateTriwulan);
+
+    function updateTriwulan() {
+        const bulan = parseInt(document.getElementById('filter_bulan').value);
+        const triwulanSelect = document.getElementById('filter_triwulan');
+        let triwulan;
+
+        if (bulan >= 1 && bulan <= 3) {
+            triwulan = 1;
+        } else if (bulan >= 4 && bulan <= 6) {
+            triwulan = 2;
+        } else if (bulan >= 7 && bulan <= 9) {
+            triwulan = 3;
+        } else if (bulan >= 10 && bulan <= 12) {
+            triwulan = 4;
+        }
+
+        triwulanSelect.value = triwulan;
+        triwulanSelect.style.display = 'block';
+    }
+    updateTriwulan();
+</script>
