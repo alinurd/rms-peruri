3f068eaca07ccf3fcd3214e9624bc845ef175785
teguh triatmojo <teguhtriatmojo23@gmail.com>
2024-10-31 23:15:57 +0700

    cek bug

--- _public/modules/modul_rcsa/rcsa/views/fom_peristiwa.php
+++ _public/modules/modul_rcsa/rcsa/views/fom_peristiwa.php
@@ -632,7 +632,7 @@ if ($field['iskri'] == 0) {
                         if (!$isi_owner_no_action_accountable) {
                             $isi_owner_no_action_accountable[] = $owner['rcsa_owner_no'];
                         }
-
+                        doi::dump($field);
 
                         ?>
 
