f16d8e6afbfc9de5d9902d7564499029732435e6
teguh triatmojo <teguhtriatmojo23@gmail.com>
2024-10-31 23:57:07 +0700

    update position fa-times-circle danger di progress treatment

--- _public/modules/modul_rcsa/progress_treatment/models/Data.php
+++ _public/modules/modul_rcsa/progress_treatment/models/Data.php
@@ -240,7 +240,7 @@ if($rows){
 
         
         if (!$monthbefore && $month !=1) {
-			$result = '<td colspan="2"><center><i class="  fa fa-times-circle text-danger"></i></center></td>';
+			$result = '<td colspan="2" style="vertical-align:middle;"><center><i class="  fa fa-times-circle text-danger"></i></center></td>';
 		} else {
             if (!$monthly) {
                 $result = '<td colspan="2" style="vertical-align:middle;"><center><i class="  fa fa-times-circle text-warning" title="Level Risiko belum lengkap"></i></center></td>';
