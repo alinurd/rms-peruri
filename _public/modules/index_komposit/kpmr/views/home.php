32a0009fad54f667c123ffeb6b6ef12e54610fc9
ali dev14 <ali.nrdn14005@gmail.com>
2024-11-04 12:41:25 +0700

    ok

--- _public/modules/index_komposit/kpmr/views/home.php
+++ _public/modules/index_komposit/kpmr/views/home.php
@@ -1,6 +1,3 @@
-    <div>
-        <span class="btn btn-primary btn-sm" style="bottom: 30px; right: 15px; cursor:pointer; position: fixed; padding: 5px 15px; z-index: 1000;"><i class="fa fa-floppy-o" aria-hidden="true"></i> Simpan </span>
-    </div>
     <h3>Parameter Kualitas Penerapan Manajemen Risiko (KPMR)</h3>
     <table class="display table table-bordered" id="tbl_event">
         <thead>
@@ -42,20 +39,19 @@
                             <?php if (count($countDetail) > 0) : ?>
                             colspan="8"
                             <?php endif; ?>>
-                            <?= $pk['parameter']; ?>                        
+                            <?= $pk['parameter']; ?>
                         </td>
                         <?php if (count($countDetail) === 0) : ?>
                             <td width="5%"><?= $pk['skala']; ?></td>
                             <td width="5%"><?= $pk['penilaian']; ?></td>
                             <?php if ($pKey === 0) : ?>
                                 <td width="5%" rowspan="<?= count($c['parent']) ?>">
-                                    <select class="form-control skala-dropdown" id="skala-<?= $pk['urut']; ?><?= $pk['id']; ?>"  style="width: 110px;"
+                                    <select class="form-control skala-dropdown" id="skala-<?= $pk['urut']; ?><?= $pk['id']; ?>" style="width: 110px;"
                                         data-bobot="<?= $pk['bobot']; ?>"
                                         data-id-parent="<?= $pk['id']; ?>"
-                                         data-input-id="perhitungan-<?= $pk['urut']; ?><?= $pk['id']; ?>" 
-                                        data-input-rumus-id="rumus-<?= $pk['urut']; ?><?= $pk['id']; ?>"
-                                        >
-                                        <option  selected  value="0" data-bobot="0" data-penilaian="0"> -Skala- </option>
+                                        data-input-id="perhitungan-<?= $pk['urut']; ?><?= $pk['id']; ?>"
+                                        data-input-rumus-id="rumus-<?= $pk['urut']; ?><?= $pk['id']; ?>">
+                                        <option selected value="0" data-bobot="0" data-penilaian="0"> -Skala- </option>
                                         <?php foreach ($c['parent'] as $option) : ?>
                                             <option value="<?= $option['skala']; ?>"
                                                 data-bobot="<?= $pk['bobot']; ?>"
@@ -65,19 +61,19 @@
                                         <?php endforeach; ?>
                                     </select>
                                 </td>
-                                <td  rowspan="<?= count($c['parent']) ?>">
-                                   <center>
-                                   <input class="form-control" style="width: 100px; text-align: center" type="text" id="rumus-<?= $pk['urut']; ?><?= $pk['id']; ?>" name="rumus-<?= $pk['urut']; ?><?= $pk['id']; ?>" readonly>
-                                   </center>
+                                <td rowspan="<?= count($c['parent']) ?>">
+                                    <center>
+                                        <input class="form-control" style="width: 100px; text-align: center" type="text" id="rumus-<?= $pk['urut']; ?><?= $pk['id']; ?>" name="rumus-<?= $pk['urut']; ?><?= $pk['id']; ?>" readonly>
+                                    </center>
                                 </td>
-                                <td  rowspan="<?= count($c['parent']) ?>">
+                                <td rowspan="<?= count($c['parent']) ?>">
                                     <center>
                                         <input class="form-control perhitungan-<?= $pk['id']; ?>" style="width: 100px; text-align: center;" type="text" id="perhitungan-<?= $pk['urut']; ?><?= $pk['id']; ?>" name="perhitungan-<?= $pk['urut']; ?><?= $pk['id']; ?>" readonly>
                                     </center>
                                 </td>
                             <?php endif; ?>
 
-                             <td width="5%"><textarea name="penjelasan" id="penjelasan"></textarea></td>
+                            <td width="5%"><textarea name="penjelasan" id="penjelasan"></textarea></td>
                             <td width="5%"><textarea name="evidence" id="evidence"></textarea></td>
                         <?php endif; ?>
                     </tr>
@@ -86,24 +82,24 @@
                     $totalPenilaian = 0;
                     $totalPenilaianCombo = 0;
                     $n = 1;
-                     foreach ($countDetail as $dKey => $d) :
-                         $totalPenilaian += $d['penilaian'];
+                    foreach ($countDetail as $dKey => $d) :
+                        $totalPenilaian += $d['penilaian'];
                         $totalPenilaianCombo += $d['penilaian'];
                     ?>
                         <tr>
                             <td width="40%"><?= $n++; ?>.&nbsp; <?= $d['parameter']; ?></td>
                             <td width="5%"><?= $d['skala']; ?></td>
                             <td width="5%"><?= $d['penilaian']; ?></td>
-                            
+
                             <?php if ($dKey === 0) : ?>
                                 <td width="5%" rowspan="<?= count($countDetail) ?>">
-                                    <select class="form-control skala-dropdown" id="skala-<?= $pk['urut']; ?><?= $d['id']; ?>"style="width: 110px;"
+                                    <select class="form-control skala-dropdown" id="skala-<?= $pk['urut']; ?><?= $d['id']; ?>" style="width: 110px;"
                                         data-bobot="<?= $pk['bobot']; ?>"
                                         data-urut="<?= $d['urut']; ?>"
                                         data-id-parent="<?= $d['parent']; ?>"
                                         data-input-id="perhitungan-<?= $pk['urut']; ?><?= $d['id']; ?>"
-                                         data-input-rumus-id="rumus-<?= $pk['urut']; ?><?= $d['id']; ?>">
-                                         <option  selected  value="0" data-bobot="0" data-penilaian="0"> -Skala- </option>
+                                        data-input-rumus-id="rumus-<?= $pk['urut']; ?><?= $d['id']; ?>">
+                                        <option selected value="0" data-bobot="0" data-penilaian="0"> -Skala- </option>
                                         <?php foreach ($countDetail as $option) : ?>
                                             <option value="<?= $option['skala']; ?>"
                                                 data-bobot="<?= $pk['bobot']; ?>"
@@ -113,20 +109,20 @@
                                         <?php endforeach; ?>
                                     </select>
                                 </td>
-                                <td  rowspan="<?= count($countDetail) ?>">
-                                <center>
-                                   <input class="form-control" style="width: 100px; text-align: center" type="text" id="rumus-<?= $pk['urut']; ?><?= $d['id']; ?>" name="rumus-<?= $pk['urut']; ?><?= $pk['id']; ?>" readonly>
-                                   </center>
+                                <td rowspan="<?= count($countDetail) ?>">
+                                    <center>
+                                        <input class="form-control" style="width: 100px; text-align: center" type="text" id="rumus-<?= $pk['urut']; ?><?= $d['id']; ?>" name="rumus-<?= $pk['urut']; ?><?= $pk['id']; ?>" readonly>
+                                    </center>
                                 </td>
-                                <td  rowspan="<?= count($countDetail) ?>">
+                                <td rowspan="<?= count($countDetail) ?>">
 
-                                <center>
-                                   <input class="form-control subTotalDetail-<?= $d['parent']; ?>" style="width: 100px; text-align: center" type="text" id="perhitungan-<?= $pk['urut']; ?><?= $d['id']; ?>" name="perhitungan-<?= $pk['urut']; ?><?= $pk['id']; ?>" readonly>
-                                   </center>
+                                    <center>
+                                        <input class="form-control subTotalDetail-<?= $d['parent']; ?>" style="width: 100px; text-align: center" type="text" id="perhitungan-<?= $pk['urut']; ?><?= $d['id']; ?>" name="perhitungan-<?= $pk['urut']; ?><?= $pk['id']; ?>" readonly>
+                                    </center>
                                 </td>
                             <?php endif; ?>
-                    
-                             <td width="5%">
+
+                            <td width="5%">
                                 <textarea name="penjelasan[]" id="penjelasan<?= $n; ?>" placeholder="Masukkan penjelasan"></textarea>
                             </td>
                             <td width="5%">
@@ -137,23 +133,50 @@
                 <?php endforeach; ?>
                 <?php if (count($countDetail) > 0) : ?>
                     <tr>
-                        <td class="text-center text-right" style="text-align: right;" colspan="6"><strong>Total Point <?=$nc?>:  </strong></td>
+                        <td class="text-center text-right" style="text-align: right;" colspan="6"><strong>Total Point <?= $nc ?>: </strong></td>
                         <td class="text-center">
                             <input class="form-control perhitungan-<?= $pk['id']; ?>" style="width: 100px; text-align: center" type="text" id="totalDetail-<?= $pk['id']; ?>" name="totalDetail-<?= $pk['id']; ?>" readonly>
                         </td>
                     </tr>
                 <?php endif; ?>
             <?php
-        $nc++; endforeach; ?>
+                $nc++;
+            endforeach; ?>
         </tbody>
 
         <tr style="position: sticky; bottom: 0; background: #367FA9; color:#fff; z-index: 1;">
-            <th class="text-center" style="font-weight:bold; color:#fff;" colspan="9">Hasil Perhitungan Indikator: </th>
-            <th class="text-center" style="background:yellow; color:#000;">
-            <input class="form-control " style="width: 100px; text-align: center" type="hidden" id="totalPerhitungan" name="totalPerhitungan" readonly>
+    <th class="text-center" style="font-weight:bold; color:#fff;" colspan="7">Hasil Perhitungan Indikator KPMR : </th>
+    <th class="text-center" style="background:yellow; color:#000;">
+        <span id="totalPerhitunganText">0</span>
+        <input class="form-control " type="hidden" id="totalPerhitungan" name="totalPerhitungan" readonly>
+    </th>
+    <th class="text-center" colspan="2">
+        <!-- Tombol Simpan dengan gaya yang lebih menarik -->
+        <button class="btn btn-save" onclick="simpanData()">
+            <i class="fa fa-floppy-o" aria-hidden="true"></i> Simpan
+        </button>
+    </th>
+</tr>
+<style>
+    .btn-save {
+        background-color: #fff;
+        color: #367FA9;
+        font-weight: bold;
+         border: none;
+        border-radius: 5px;
+        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2);
+        cursor: pointer;
+        transition: background-color 0.3s, box-shadow 0.3s;
+    }
+     .btn-save:hover {
+        background-color: #28597A;
+        box-shadow: 0 6px 8px rgba(0, 0, 0, 0.3);
+    } 
+    .btn-save:active {
+        background-color: #204562;
+        box-shadow: 0 3px 5px rgba(0, 0, 0, 0.4);
+    }
+</style>
 
-                <span id="totalPerhitunganText"></span>
-            </th>
-        </tr>
     </table>
     </div>
