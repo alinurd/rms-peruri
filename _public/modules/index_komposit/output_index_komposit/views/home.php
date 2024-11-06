6f4870c01ce0c19a34c819563359118157af95c4
ali dev14 <ali.nrdn14005@gmail.com>
2024-11-06 14:26:33 +0700

    started output komposit

---
+++ _public/modules/index_komposit/output_index_komposit/views/home.php
@@ -0,0 +1,181 @@
+<section class="x_panel">
+    <div class="x_title">
+        <h3>Output Index Komposit</h3>
+
+        <div class="clearfix"></div>
+    </div>
+    <div class="x_content">
+        <div class="row">
+            <div class="col-md-6">
+                <section class="x_panel">
+                    <div class="x_title">
+                        <strong>Kualitas Penerapan Management Risko (KPMR)</strong>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="x_content">
+                        <div class="row">
+                            <div class="clearfix"></div>
+                            <div class="table-responsive">
+                                <table class="display table table-bordered" id="tbl_event">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Indikator</th>
+                                            <th>Bobot</th>
+                                            <th>Hasil Penilaian</th>
+                                            <th>Skor Penilaian</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>No</td>
+                                            <td>Indikator</td>
+                                            <td>Bobot</td>
+                                            <td>Hasil Penilaian</td>
+                                            <td>Skor Penilaian</td>
+                                        </tr>
+                                        <tr style="background: #367FA9; color:#fff;">
+                                            <th colspan="4" style="text-align: center;">Total Nilai</th>
+                                            <th>83</th>
+                                        </tr>
+                                        <tr style="background: #367FA9; color:#fff;">
+                                            <th colspan="4" style="text-align: center;">Kualitas Penerapan Management Risko (KPMR)</th>
+                                            <th>Fair</th>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+
+            <div class="col-md-6">
+                <section class="x_panel">
+                    <div class="x_title">
+                        <strong>Pencapaian Kinerja</strong>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="x_content">
+                        <div class="row">
+                            <div class="clearfix"></div>
+                            <div class="table-responsive">
+                                <table class="display table table-bordered" id="tbl_event">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Indikator</th>
+                                            <th>Bobot</th>
+                                            <th>Hasil Penilaian</th>
+                                            <th>Skor Penilaian</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>No</td>
+                                            <td>Indikator</td>
+                                            <td>Bobot</td>
+                                            <td>Hasil Penilaian</td>
+                                            <td>Skor Penilaian</td>
+                                        </tr>
+                                        <tr style="background: #367FA9; color:#fff;">
+                                            <th colspan="4" style="text-align: center;">Total Nilai</th>
+                                            <th>83</th>
+                                        </tr>
+                                        <tr style="background: #367FA9; color:#fff;">
+                                            <th colspan="4" style="text-align: center;">Kinerja</th>
+                                            <th>Fair</th>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+
+        <!-- MAP Komposit -->
+        <div class="row">
+            <div class="col-md-12">
+                <section class="x_panel"> 
+                    <div class="x_content">
+                        <div class="row">
+                            <div class="clearfix"></div>
+                           <center>
+                            <strong>MAP</strong>
+                           </center>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+        <!-- endMAP Komposit -->
+
+        <div class="row">
+            <div class="col-md-6">
+                <section class="x_panel">
+                    <div class="x_title">
+                        <i>Conversi Skor Penilaian Terhadap KPMR</i>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="x_content">
+                        <div class="row">
+                            <div class="clearfix"></div>
+                            <div class="table-responsive">
+                                <table class="display table table-bordered" id="tbl_event">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>KPMR</th> 
+                                            <th>Skor Penilaian</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>No</td>
+                                            <td>Indikator</td> 
+                                            <td>Skor Penilaian</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+
+            <div class="col-md-6">
+                <section class="x_panel">
+                    <div class="x_title">
+                    <i>Conversi Skor Penilaian Terhadap Pencapaian Kinerja</i>
+                    <div class="clearfix"></div>
+                    </div>
+                    <div class="x_content">
+                        <div class="row">
+                            <div class="clearfix"></div>
+                            <div class="table-responsive">
+                            <table class="display table table-bordered" id="tbl_event">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Kinerja</th> 
+                                            <th>Skor Penilaian</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>No</td>
+                                            <td>Indikator</td> 
+                                            <td>Skor Penilaian</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+</section>
