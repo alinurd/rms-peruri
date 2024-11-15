7ea162b66b4aac8bebad3df4b6c196892403c16a
teguh triatmojo <teguhtriatmojo23@gmail.com>
2024-11-15 14:42:58 +0700

    update fix bug search appetite dan rcm

--- _public/modules/modul_rcsa/rcsa_appetite/controllers/Rcsa_Appetite.php
+++ _public/modules/modul_rcsa/rcsa_appetite/controllers/Rcsa_Appetite.php
@@ -27,10 +27,10 @@ class Rcsa_Appetite extends BackendController
 		$this->addField(array('field' => 'id', 'type' => 'int', 'show' => false, 'size' => 4));
 
 		$this->addField(array('field' => 'judul_assesment', 'show' =>false));
-		// $this->addField(array('field' => 'owner_no', 'input' => 'combo:search', 'combo' => $this->cbo_parent, 'size' => 100, 'required' => true, 'search' => true));
-		// $this->addField(array('field' => 'officer_no', 'show' => false, 'save' => true, 'default' => $this->authentication->get_info_user('identifier')));
-		// $this->addField(array('field' => 'create_user', 'search' => false, 'default' => $this->authentication->get_info_user('username')));
-		// $this->addField(array('field' => 'period_no', 'input' => 'combo', 'combo' => $this->cbo_periode, 'size' => 15, 'search' => true, 'required' => false));
+		$this->addField(array('field' => 'owner_no', 'input' => 'combo:search', 'combo' => $this->cbo_parent, 'size' => 100, 'required' => true, 'search' => true));
+		$this->addField(array('field' => 'officer_no', 'show' => false, 'save' => true, 'default' => $this->authentication->get_info_user('identifier')));
+		$this->addField(array('field' => 'create_user', 'search' => false, 'default' => $this->authentication->get_info_user('username')));
+		$this->addField(array('field' => 'period_no', 'input' => 'combo', 'combo' => $this->cbo_periode, 'size' => 15, 'search' => true, 'required' => false));
 		$this->addField(array('field' => 'sasaran', 'title' => 'Risk Appetite', 'type' => 'free', 'input' => 'free', 'mode' => 'e'));
 
 		// $this->addField(array('field' => 'anggaran_rkap', 'type' => 'float', 'input' => 'float', 'required' => true));
