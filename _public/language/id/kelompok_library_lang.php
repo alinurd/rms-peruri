a4a3780af9b0fd60d1b006073bdf3d7cce950a3b
ali dev14 <ali.nrdn14005@gmail.com>
2024-12-17 16:15:12 +0700

    update kel_lib

---
+++ _public/language/id/kelompok_library_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['msg_title']					= "Kelompok Risiko - T3";
+$lang['msg_field_description']		= "Kelompok Risiko"; 
+
+
+/* Keterangan Help */
+$lang['msg_help_photo']			= "Code"; 
+
+
+/* End of file form_lang.php */
+/* Location: ./application/language/english/form_lang.php */
