18a484a1a566f006b383be8e825920c2bc999fa7
teguh triatmojo <teguhtriatmojo23@gmail.com>
2024-12-18 15:55:50 +0700

    ok update detail propose submit risk register

--- _public/modules/modul_rcsa/rcsa/views/register.php
+++ _public/modules/modul_rcsa/rcsa/views/register.php
@@ -211,13 +211,12 @@ if ($status) : ?>
 	<table class="table table-bordered table-sm test" id="datatables_event" border="1">
     <thead>
             
-
             <tr>
                 <th rowspan="3">No</th>
                 <th rowspan="3"><label class="w100">Sasaran (Objective)</label></th>
                 <th rowspan="3"><label class="w100">Tema Risiko (Risk Theme) T1</label></th>
                 <th rowspan="3"><label class="w100">Kategori Risiko (Risk Category) T2</label></th>
-                <th rowspan="3"><label class="w100">Sub Kategori Risiko</label></th>
+                <th rowspan="3"><label class="w100">Sub Kategori Risiko T3</label></th>
                 <th rowspan="3"><label class="w100">Proses Bisnis (Business Process)</label></th>
 
                 <th colspan="6"><label>Identifikasi Risiko (Risk Identification)</label></th>
@@ -385,14 +384,16 @@ if ($status) : ?>
                 $act                = $this->db
                                             ->where('rcsa_detail_no', $row['id'])
                                             ->get('bangga_rcsa_action')->row_array();
+                                            // doi::dump($row);
 
 ?>
                 <tr>
                     <td valign="top" style="text-align: center;"><?= $no++; ?></td>
                     <td valign="top"><?= $row['sasaran']; ?> </td>
+                    <td valign="top"><?= $row['tema_risiko']; ?> </td>
                     <td valign="top"><?= $tema['description']; ?></td>
                     <td valign="top"><?= $row['kategori']; ?></td>
-                    <td valign="top"><?= ($row['subrisiko'] == 1) ? 'negatif' : 'positif' ?></td>
+                    <!-- <td valign="top"><?= ($row['subrisiko'] == 1) ? 'negatif' : 'positif' ?></td> -->
                     <td valign="top"><?= $row['proses_bisnis']; ?></td>
 
                     <td valign="top"><?= $row['event_name']; ?></td>
@@ -410,7 +411,7 @@ if ($status) : ?>
                     <td valign="top" style="text-align: left; "><?= 'Rp....'; ?></td>
 
                     <td valign="top"><?= format_list($row['control_name'], "### "); ?></td>
-                    <td valign="top"><?= number_format($row['anggaran'],0,',','.') ?></td>
+                    <td valign="top"><?= number_format($row['coa'],0,',','.') ?></td>
                     <td valign="top"><?= $row['kode_jasa']; ?></td>
                     <td valign="top"><?= $control_as['data']; ?></td>
                     <td valign="top"><?= $row['treatment']; ?></td>
