39ce5f4098b8d2d0e243f8c67063acf66259779b
teguh triatmojo <teguhtriatmojo23@gmail.com>
2024-12-23 10:21:02 +0700

    update monitoring treatment report

--- _public/modules/modul_report/monitoring_treatment/models/Data.php
+++ _public/modules/modul_report/monitoring_treatment/models/Data.php
@@ -8,105 +8,196 @@ class Data extends MX_Model {
         parent::__construct();
 	}
 
-    function grafik (){
-        $result=[];
-        $rows=$this->db->order_by('urut')->get(_TBL_STATUS_ACTION)->result_array();
-        $master=[];
-        foreach($rows as $row){
-            $master[$row['id']]=['name'=>$row['status_action'], 'color'=>$row['warna'], 'jml'=>0];
-        }
+//     function grafik (){
+//         $result=[];
+//         $rows=$this->db->order_by('urut')->get(_TBL_STATUS_ACTION)->result_array();
+//         $master=[];
+//         foreach($rows as $row){
+//             $master[$row['id']]=['name'=>$row['status_action'], 'color'=>$row['warna'], 'jml'=>0];
+//         }
         
-        $this->owner_child=array();
+//         $this->owner_child=array();
 
-		if ($this->post['owner_no']>0){
-			$this->owner_child[]=$this->post['owner_no'];
-		}
+// 		if ($this->post['owner_no']>0){
+// 			$this->owner_child[]=$this->post['owner_no'];
+// 		}
 
-		$this->get_owner_child($this->post['owner_no']);
-		$owner_child=$this->owner_child;
+// 		$this->get_owner_child($this->post['owner_no']);
+// 		$owner_child=$this->owner_child;
 
-		if ($owner_child){
-			$this->db->where_in('owner_no',$owner_child);
-		}else{
-			$this->db->where('owner_no',$this->post['owner_no']);
-		}
-		
-		// if ($this->post['risk_context']){
-		// 	$this->db->where_in('rcsa_no',$this->post['risk_context']);
-  //       }
-        // if ($this->post['owner_no']){
-        //     $this->db->where('owner_no',$this->post['owner_no']);
-        // }
-         if ($this->post['periode_no']){
-            $this->db->where('period_no',$this->post['periode_no']);
-        }
+// 		if ($owner_child){
+// 			$this->db->where_in('owner_no',$owner_child);
+// 		}else{
+// 			$this->db->where('owner_no',$this->post['owner_no']);
+// 		}
 
-        if ($this->post['bulan']){
-			$this->db->where('bulan',$this->post['bulan']);
-        }
+//          if ($this->post['periode_no']){
+//             $this->db->where('period_no',$this->post['periode_no']);
+//         }
 
-        $rows=$this->db->select('status_no, status_action_detail, count(status_no) as jml')->group_by(['status_no', 'status_action_detail'])->get(_TBL_VIEW_RCSA_ACTION_DETAIL)->result_array();
+//         if ($this->post['bulan']){
+// 			$this->db->where('bulan',$this->post['bulan']);
+//         }
 
-        foreach($master as $key=>$mr){
-            foreach($rows as $row){
-                if ($row['status_no']==$key){
-                    $master[$key]['jml']=$row['jml'];
-                }
-            }
-        }
-        $result['master']=$master;
+//         $rows=$this->db->select('status_no, status_action_detail, count(status_no) as jml')->group_by(['status_no', 'status_action_detail'])->get(_TBL_VIEW_RCSA_ACTION_DETAIL)->result_array();
+
+//         foreach($master as $key=>$mr){
+//             foreach($rows as $row){
+//                 if ($row['status_no']==$key){
+//                     $master[$key]['jml']=$row['jml'];
+//                 }
+//             }
+//         }
+//         $result['master']=$master;
    
 
-        // setelah progress
-        $rows=$this->db->order_by('urut')->get(_TBL_STATUS_ACTION)->result_array();
-        $master=[];
+//         // setelah progress
+//         $rows=$this->db->order_by('urut')->get(_TBL_STATUS_ACTION)->result_array();
+//         $master=[];
         
-        foreach($rows as $row){
-            $master[$row['id']]=['name'=>$row['status_action'], 'color'=>$row['warna'], 'jml'=>0];
-        }
+//         foreach($rows as $row){
+//             $master[$row['id']]=['name'=>$row['status_action'], 'color'=>$row['warna'], 'jml'=>0];
+//         }
    
         
-        $this->owner_child=array();
+//         $this->owner_child=array();
 
-		if ($this->post['owner_no']>0){
-			$this->owner_child[]=$this->post['owner_no'];
-		}
+// 		if ($this->post['owner_no']>0){
+// 			$this->owner_child[]=$this->post['owner_no'];
+// 		}
 
-		$this->get_owner_child($this->post['owner_no']);
-		$owner_child=$this->owner_child;
+// 		$this->get_owner_child($this->post['owner_no']);
+// 		$owner_child=$this->owner_child;
 
-		if ($owner_child){
-			$this->db->where_in('owner_no',$owner_child);
-		}else{
-			$this->db->where('owner_no',$this->post['owner_no']);
-		}
+// 		if ($owner_child){
+// 			$this->db->where_in('owner_no',$owner_child);
+// 		}else{
+// 			$this->db->where('owner_no',$this->post['owner_no']);
+// 		}
 		
-		// if ($this->post['risk_context']){
-		// 	$this->db->where_in('rcsa_no',$this->post['risk_context']);
-  //       }
-        if ($this->post['owner_no']){
-            $this->db->where('owner_no',$this->post['owner_no']);
-        }
-         if ($this->post['periode_no']){
-            $this->db->where('period_no',$this->post['periode_no']);
-        }
-        if ($this->post['bulan']){
-			$this->db->where('bulan',1);
-        }
+// 		// if ($this->post['risk_context']){
+// 		// 	$this->db->where_in('rcsa_no',$this->post['risk_context']);
+//   //       }
+//         if ($this->post['owner_no']){
+//             $this->db->where('owner_no',$this->post['owner_no']);
+//         }
+//          if ($this->post['periode_no']){
+//             $this->db->where('period_no',$this->post['periode_no']);
+//         }
+//         if ($this->post['bulan']){
+// 			$this->db->where('bulan',1);
+//         }
         
-        $rows=$this->db->select('status_no, status_action_detail, count(status_no) as jml')->group_by(['status_no', 'status_action_detail'])->get(_TBL_VIEW_RCSA_ACTION_DETAIL)->result_array();
+//         $rows=$this->db->select('status_no, status_action_detail, count(status_no) as jml')->group_by(['status_no', 'status_action_detail'])->get(_TBL_VIEW_RCSA_ACTION_DETAIL)->result_array();
 
-        foreach($master as $key=>$mr){
-            foreach($rows as $row){
-                if ($row['status_no']==$key){
-                    $master[$key]['jml']=$row['jml'];
-                }
-            }
-        }
-        $result['master2']=$master;
+//         foreach($master as $key=>$mr){
+//             foreach($rows as $row){
+//                 if ($row['status_no']==$key){
+//                     $master[$key]['jml']=$row['jml'];
+//                 }
+//             }
+//         }
+//         $result['master2']=$master;
 
-        return $result;
+//         return $result;
+//     }
+
+function grafik() {
+    $result = [];
+
+    // Dapatkan owner_child berdasarkan owner_no
+    $this->owner_child = [];
+    if ($this->post['owner_no'] > 0) {
+        $this->owner_child[] = $this->post['owner_no'];
     }
+
+    $this->get_owner_child($this->post['owner_no']);
+    $owner_child = $this->owner_child;
+
+    // Filter berdasarkan owner_no
+    if ($owner_child) {
+        $this->db->where_in('owner_no', $owner_child);
+    } else {
+        $this->db->where('owner_no', $this->post['owner_no']);
+    }
+
+    // Filter berdasarkan periode dan bulan
+    if (!empty($this->post['periode_no'])) {
+        $this->db->where('period_no', $this->post['periode_no']);
+    }
+
+    // Query untuk kategori "Kurang", "Sama", "Lebih"
+    $rows = $this->db->select(
+        "b.bulan, 
+        c.owner_no, 
+        c.period_no, 
+        CASE
+            WHEN b.progress_detail < a.target_progress_detail AND  b.target_progress_detail < a.target_damp_loss THEN 'Kurang'
+            WHEN  b.progress_detail = a.target_progress_detail AND  b.target_progress_detail =  a.target_damp_loss THEN 'Sama'
+            WHEN b.progress_detail > a.target_progress_detail  AND b.target_progress_detail  > a.target_damp_loss THEN 'Lebih'
+        END AS kategori, 
+        COUNT(*) AS jml",
+        false
+    )
+    ->from('bangga_rcsa_treatment a')
+    ->join('bangga_rcsa_monitoring_treatment b', 'a.rcsa_detail_no = b.rcsa_detail AND a.bulan = b.bulan', 'inner') // Pastikan bulan sama
+    ->join('bangga_view_rcsa_detail c', 'b.rcsa_detail = c.id', 'left')
+    ->group_by([
+        'b.bulan',
+        'c.owner_no',
+        'kategori',
+        'a.target_progress_detail',
+        'a.target_damp_loss',
+        'b.progress_detail',
+        'b.target_progress_detail',
+        'c.period_no'
+    ])
+    ->order_by('b.bulan', 'ASC')
+    ->get()
+    ->result_array();
+
+
+    // Daftar bulan dari Januari hingga Desember dengan key indeks 1-12
+    $bulan = [
+        1 => 'Jan',
+        2 => 'Feb',
+        3 => 'Mar',
+        4 => 'Apr',
+        5 => 'May',
+        6 => 'Jun',
+        7 => 'Jul',
+        8 => 'Aug',
+        9 => 'Sep',
+        10 => 'Oct',
+        11 => 'Nov',
+        12 => 'Dec'
+    ];
+
+    // Proses hasil query
+    $dataKategori = [];
+    foreach ($rows as $row) {
+        $bulanKey = $bulan[(int)$row['bulan']] ?? null;
+        $kategori = $row['kategori'];
+        $jml = $row['jml'];
+
+        if (!isset($dataKategori[$kategori])) {
+            $dataKategori[$kategori] = array_fill_keys(array_values($bulan), 0);
+        }
+
+        if ($bulanKey) {
+            $dataKategori[$kategori][$bulanKey] = $jml;
+        }
+    }
+
+    // Format hasil untuk master
+    $result['master'] = $dataKategori;
+
+    return $result;
+}
+
+
+
+
 }
 /* End of file app_login_model.php */
 /* Location: ./application/models/app_login_model.php */
--- _public/modules/modul_report/monitoring_treatment/views/grafik.php
+++ _public/modules/modul_report/monitoring_treatment/views/grafik.php
@@ -1,116 +1,172 @@
-<div id="coba"> 
-<table class="table table-bordered table-sm"  id="golum" border="5" style="display: none;">
-    <!-- <table class="table table-bordered table-sm"  id="golum" border="5" > -->
-<thead>
-<tr>
-<td colspan="3" rowspan="3" style="text-align: left;border-right:none;" ><img src="<?=img_url('logo.png');?>" width="90"></td>
-<td colspan="6" rowspan="3" style="text-align: center;border-left:none;vertical-align: middle !important;"> RISK MONITORING TREATMENT</td>
-<td style="text-align: left;">No.</td>
-<td style="text-align: left;">: 012/RM-FORM/I/<?= $post['tahun']; ?></td>
-</tr>
-<tr>
-<td style="text-align: left;">Revisi</td>
-<td style="text-align: left;">: 1</td>
-</tr>
-<tr>
-<td style="text-align: left;">Tanggal Revisi</td>
-<td style="text-align: left;">: 31 Januari <?= $post['tahun']; ?></td>
-</tr>
-<tr>
-<td colspan="2" style="text-align: left;">Risk Owner </td>
-<td colspan="9" style="text-align: left;">: <?= $post['owner_name']; ?></td>
-</tr>
-<tr>
-<td colspan="2" style="text-align: left;">Bulan </td>
-<td colspan="9" style="text-align: left;">: <?= $post['bulan2']; ?></td>
-</tr>
-</thead>
-</table>
-
-<div class="col-md-12 col-sm-6 col-xs-6">
-    <center>
-    <canvas id="mybarChart"></canvas>
-    <br/>&nbsp;<hr>
-    <table class="table" width="90%">
+<div id="coba">
+    <table class="table table-bordered table-sm" id="golum" border="5" style="display: none;">
         <thead>
             <tr>
-                <th width="70%">Treatment</th>
-                <th>Jumlah</th>
-            </tr>
-        <thead>
-        <tbody>
-            <?php
-            foreach($master as $row):?>
-            <tr>
-                <td> 
-                    <?php if ($row['name'] == "Add") : ?>
-                    <span style="background-color:red;color:white;">&nbsp;Add &nbsp;</span>
-                    <?php else: ?>
-                    <span style="background-color:blue;color:white;">&nbsp;<?= $row['name']; ?>&nbsp;</span>
-                <?php endif; ?>
-
+                <td colspan="3" rowspan="3" style="text-align: left; border-right: none;">
+                    <img src="<?= img_url('logo.png'); ?>" width="90">
                 </td>
-                <td class="text-center"><?=$row['jml']?></td>
+                <td colspan="6" rowspan="3" style="text-align: center; border-left: none; vertical-align: middle;">
+                    RISK MONITORING TREATMENT
+                </td>
+                <td style="text-align: left;">No.</td>
+                <td style="text-align: left;">: 012/RM-FORM/I/<?= $post['tahun']; ?></td>
             </tr>
-            <?php endforeach;?>
-        </tbody>
+            <tr>
+                <td style="text-align: left;">Revisi</td>
+                <td style="text-align: left;">: 1</td>
+            </tr>
+            <tr>
+                <td style="text-align: left;">Tanggal Revisi</td>
+                <td style="text-align: left;">: 31 Januari <?= $post['tahun']; ?></td>
+            </tr>
+            <tr>
+                <td colspan="2" style="text-align: left;">Risk Owner</td>
+                <td colspan="9" style="text-align: left;">: <?= $post['owner_name']; ?></td>
+            </tr>
+            <tr>
+                <td colspan="2" style="text-align: left;">Bulan</td>
+                <td colspan="9" style="text-align: left;">: <?= $post['bulan2']; ?></td>
+            </tr>
+        </thead>
     </table>
-    </center>
-</div>
+
+    <div class="col-md-12 col-sm-6 col-xs-6">
+        <center>
+            <canvas id="mybarChart"></canvas>
+            <br>&nbsp;<hr>
+            <table class="table" width="90%">
+                <thead>
+                    <tr>
+                        <th width="70%">Treatment</th>
+                        <th>Jumlah</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($master as $kategori => $dataBulan): ?>
+                    <?php if ($kategori !== '' && array_sum($dataBulan) > 0): // Tampilkan hanya jika kategori ada dan jumlah lebih dari 0 ?>
+                        <tr>
+                            <td>
+                                <span style="background-color: <?php 
+                                    if ($kategori === 'Kurang') echo 'rgba(255, 99, 71, 0.7)'; // Merah lembut
+                                    elseif ($kategori === 'Sama') echo 'rgb(148, 194, 255)'; // Biru lembut
+                                    elseif ($kategori === 'Lebih') echo 'rgba(144, 238, 144, 0.7)'; // Hijau lembut
+                                    else echo '#ccc'; // Default abu-abu
+                                ?>; color: white;">&nbsp;<?= $kategori; ?>&nbsp;</span>
+                            </td>
+                            <td class="text-center">
+                                <?= array_sum($dataBulan); // Menjumlahkan total data per kategori ?>
+                            </td>
+                        </tr>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+
+
+                </tbody>
+            </table>
+        </center>
+    </div>
 </div>
 <?php
-    $labels=[];
-    $nils=[];
-    $colors=[];
-    foreach ($master as $key=>$row){
-        $labels[]=$row['name'];
-        $nils[]=$row['jml'];
-        $colors[]=$row['color'];
-    }
-    $data['data'] = [
-            'labels'=>$labels,
-            'datasets'=>
-            [
-                [
-                'label'=>'Total',
-                'data'=> $nils,
-                'backgroundColor'=> $colors,
-                ]
-            ]
-            ];
-    $data['judul']=[$post['title_text'], $bulan[$post['bulan']].' '.$periode[$post['periode_no']]];
-    $data = json_encode($data);
 
-    $labels=[];
-    $nils=[];
-    $colors=[];
-    foreach ($master2 as $key=>$row){
-        $labels[]=$row['name'];
-        $nils[]=$row['jml'];
-        $colors[]=$row['color'];
-    }
-    $data2['data'] = [
-            'labels'=>$labels,
-            'datasets'=>
-            [
-                [
-                'label'=>'Total ',
-                'data'=> $nils,
-                'backgroundColor'=> $colors,
-                ]
-            ]
-            ];
-    $data2['judul']=[$post['title_text'], $bulan[$post['bulan']].' '.$periode[$post['periode_no']]];
-    $data2 = json_encode($data2);
+// Daftar bulan dari Januari hingga Desember
+$bulan = [
+    1 => 'Jan',
+    2 => 'Feb',
+    3 => 'Mar',
+    4 => 'Apr',
+    5 => 'May',
+    6 => 'Jun',
+    7 => 'Jul',
+    8 => 'Aug',
+    9 => 'Sep',
+    10 => 'Oct',
+    11 => 'Nov',
+    12 => 'Dec',
+];
 
-    $option=['title'=>(isset($post['title']))?true:false, 'legend'=>(isset($post['label']))?true:false, 'position'=>$post['position_label'], 'type'=>$post['type_chart']];
-    $option=json_encode($option);
+// Inisialisasi array untuk setiap kategori
+$dataKategori = [
+    'Sama' => array_fill_keys(array_values($bulan), 0),
+    'Kurang' => array_fill_keys(array_values($bulan), 0),
+    'Lebih' => array_fill_keys(array_values($bulan), 0),
+];
+
+// Iterasi data master dan isi array kategori
+foreach ($master as $kategori => $dataBulan) {
+    if (isset($dataKategori[$kategori])) {
+        foreach ($dataBulan as $bulanKey => $jumlah) {
+            if (isset($dataKategori[$kategori][$bulanKey])) {
+                $dataKategori[$kategori][$bulanKey] = (int)$jumlah;
+            }
+        }
+    }
+}
+
+// Debug: Tampilkan hasil data kategori
+// doi::dump($dataKategori);
+
+// Prepare data for JavaScript
+$data = [
+    'data' => [
+        'labels' => array_values($bulan), // Label bulan
+        'datasets' => [
+            [
+                'label' => 'Kurang',
+                'data' => array_values($dataKategori['Kurang']),
+                'backgroundColor' => 'rgba(255, 99, 71, 0.7)', // Warna merah lembut
+            ],
+            [
+                'label' => 'Sama',
+                'data' => array_values($dataKategori['Sama']),
+                'backgroundColor' => 'rgb(148, 194, 255)', // Warna biru lembut
+            ],
+            [
+                'label' => 'Lebih',
+                'data' => array_values($dataKategori['Lebih']),
+                'backgroundColor' => 'rgba(144, 238, 144, 0.7)', // Warna hijau lembut
+            ],
+        ],
+    ],
+    'judul' => [$post['title_text'], 'Periode ' . $post['periode_no']],
+];
+$dataJson = json_encode($data);
+
 ?>
-
 <script>
-    var data = <?=$data;?>;
-    // var data2 = <?=$data2;?>;
-    var option=<?=$option;?>;
-    graph (data, 'mybarChart');
-    // graph (data2, 'mybarChart2');
+    const data = <?= $dataJson; ?>;
+
+    // Fungsi untuk menggambar grafik
+    function graph(chartData, chartId) {
+        const ctx = document.getElementById(chartId).getContext('2d');
+        new Chart(ctx, {
+            type: 'bar', // Grafik batang
+            data: chartData.data,
+            options: {
+                responsive: true,
+                plugins: {
+                    legend: {
+                        display: true,
+                        position: 'top',
+                    },
+                    title: {
+                        display: true,
+                        text: chartData.judul.join(' - '),
+                    },
+                },
+                scales: {
+                    x: {
+                        stacked: true, // Jika ingin bar bertumpuk
+                        beginAtZero: true,
+                    },
+                    y: {
+                        stacked: true, // Jika ingin bar bertumpuk
+                        beginAtZero: true,
+                    },
+                },
+            },
+        });
+    }
+
+    // Panggil fungsi untuk menggambar grafik
+    graph(data, 'mybarChart');
 </script>
