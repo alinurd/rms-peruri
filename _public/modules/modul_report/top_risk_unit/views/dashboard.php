6a6594cd7fb52f34242d34e2bd74d530733fe1c6
ali dev14 <ali.nrdn14005@gmail.com>
2025-02-24 12:59:50 +0700

    ganti jadi 3 map

--- _public/modules/modul_report/top_risk_unit/views/dashboard.php
+++ _public/modules/modul_report/top_risk_unit/views/dashboard.php
@@ -61,8 +61,28 @@ if ($post['project_no'] > 0)
 							</div>
 						</div>
 						<aside class="">
-							<section class=" ">
-								<div class="  text-center" id="mapping_inherent">
+						<section class="x_panel">
+						<div class="profile-info col-md-4">
+										<!-- <strong>Residual</strong> -->
+										<div class="x_content text-center" id="mapping_current">
+											<?=$mapping['current'];?>
+										</div>
+									</div>
+									<div class="profile-info col-md-4">
+										<!-- <strong>Inherent</strong> -->
+										<div class="x_content text-center" id="mapping_inherent">
+											<?=$mapping['inherent'];?>
+										</div>
+									</div>
+									<div class="profile-info col-md-4">
+										<div class="x_content text-center"  style="max-width: 100%;" id="mapping_residual">
+											<?= $mapping['residual']; ?>
+										</div>
+									</div>
+								</section>
+								
+							<section class="hide ">
+								<div class="  text-center" id="mapping_inherent"> 
 									<?= $mapping['inherent']; ?>
 								</div>
 								<div style="margin-left: 235px; margin-top: 20px; margin-bottom: 20px; text-align: center; display: flex; justify-content: center; gap: 20px;">
